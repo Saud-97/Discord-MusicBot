facd7d433a44165701a0265ad58ea183aeb6c738
saud-97 <39028181+saud-97@users.noreply.github.com>
2022-06-22 03:12:36 +0300

    Shortened the time for presence reset

--- events/ready.js
+++ events/ready.js
@@ -7,12 +7,13 @@ module.exports = (client) => {
     client.manager.init(client.user.id);
 
   const activities = client.config.presence.activities;
+
   setInterval(() => {
     const index = Math.floor(Math.random() * (activities.length - 1));
     client.user.setActivity({
       name: activities[index].name,
       type: activities[index].type
     });
-  }, 100000);
+  }, 10000);
   client.log("Successfully logged in as " + client.user.tag);
 };
