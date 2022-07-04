4fac4e19d69d0ee0cb6a0afc5eaa41f793d38810
saud-97 <39028181+saud-97@users.noreply.github.com>
2022-07-04 21:21:48 +0300

    Updated the invite permissions in config.js

--- config.js
+++ config.js
@@ -25,12 +25,12 @@ module.exports = {
 	clientId: process.env.Discord_ClientID || "", //ID of the bot
 	clientSecret: process.env.Discord_ClientSecret || "", //Client Secret of the bot
 	port: process.env.PORT || 4200, //Port of the API and Dashboard
-	scopes: ["identify", "guilds", "applications.commands"], //Discord OAuth2 Scopes
+	scopes: ["bot", "applications.commands"], //Discord OAuth2 Scopes
+	permissions: 8, //Bot Inviting Permissions
 	serverDeafen: false, //If you want bot to stay deafened
 	defaultVolume: 100, //Sets the default volume of the bot, You can change this number anywhere from 1 to 100
 	supportServer: "https://discord.gg/sbySMS7m3v", //Support Server Link
 	Issues: "https://github.com/SudhanPlayz/Discord-MusicBot/issues", //Bug Report Link
-	permissions: 277083450689, //Bot Inviting Permissions
 	disconnectTime: 30000, //How long should the bot wait before disconnecting from the voice channel. in miliseconds. set to 1 for instant disconnect.
 	alwaysPlay: false, // when set to true music will always play no matter if there's no one in voice channel.
 	debug: true, //Debug mode
